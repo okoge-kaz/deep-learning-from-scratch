b0f42a6c8fc93a73a013bf95ea237aca7e643e7a
kazuki <kaz.tokyo.tech20@gmail.com>
2023-03-28 11:56:31 +0900

    feat: add affine layer

--- deep-learning-2/ch01/forward_net.py
+++ deep-learning-2/ch01/forward_net.py
@@ -7,3 +7,18 @@ class Sigmoid:
 
     def forward(self, x: np.ndarray | float) -> np.ndarray | float:
         return 1 / (1 + np.exp(-x))
+
+
+class Affine:
+    """
+    Affine transformation layer
+    全結合層のこと
+    """
+
+    def __init__(self, W: np.ndarray, b: np.ndarray) -> None:
+        self.params: list = [W, b]
+
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        W, b = self.params
+        out = np.dot(x, W) + b
+        return out
