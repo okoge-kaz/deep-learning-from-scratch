1fcf3915982b3a6e7b2eb63fa82b18d2963011e1
kazuki <kaz.tokyo.tech20@gmail.com>
2023-03-28 12:00:47 +0900

    feat: define layer net

--- deep-learning-2/ch01/forward_net.py
+++ deep-learning-2/ch01/forward_net.py
@@ -22,3 +22,27 @@ class Affine:
         W, b = self.params
         out = np.dot(x, W) + b
         return out
+
+
+class TwoLayerNet:
+    params: list
+
+    def __init__(self, input_size: int, hidden_size: int, output_size: int) -> None:
+        INPUT_SIZE, HIDDEN_SIZE, OUTPUT_SIZE = input_size, hidden_size, output_size
+
+        # Initialize weights and biases
+        W1 = np.random.randn(INPUT_SIZE, HIDDEN_SIZE)
+        b1 = np.random.randn(HIDDEN_SIZE)
+        W2 = np.random.randn(HIDDEN_SIZE, OUTPUT_SIZE)
+        b2 = np.random.randn(OUTPUT_SIZE)
+
+        # Create layers
+        self.layers = [
+            Affine(W1, b1),
+            Sigmoid(),
+            Affine(W2, b2),
+        ]
+        self.params: list = []
+
+        for layer in self.layers:
+            self.params += layer.params
