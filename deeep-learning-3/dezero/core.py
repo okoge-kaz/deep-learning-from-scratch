5939e17f5fe419379308b1da6239c4159c9e3c6a
kazuki <kaz.tokyo.tech20@gmail.com>
2023-04-04 14:28:44 +0900

    step2: add example

--- deeep-learning-3/dezero/core.py
+++ deeep-learning-3/dezero/core.py
@@ -1,5 +1,7 @@
 from typing import Any
 
+import numpy as np
+
 
 class Variable:
     def __init__(self, data: Any):
@@ -15,3 +17,16 @@ class Function:
 
     def forward(self, x: Any) -> Any:
         raise NotImplementedError()
+
+
+class Square(Function):
+    def forward(self, x: Any) -> Any:
+        return x**2
+
+
+x = Variable(np.array(10))
+f = Square()
+y = f(x)
+
+print(type(y))
+print(y.data)
