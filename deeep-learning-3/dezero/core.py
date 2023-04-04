6665eecf89ed01689b214c19d47203a3459b5005
kazuki <kaz.tokyo.tech20@gmail.com>
2023-04-04 14:44:05 +0900

    step6

--- deeep-learning-3/dezero/core.py
+++ deeep-learning-3/dezero/core.py
@@ -5,7 +5,8 @@ import numpy as np
 
 class Variable:
     def __init__(self, data: Any):
-        self.data = data
+        self.data: Any = data
+        self.grad: Any = None
 
 
 class Function:
@@ -13,9 +14,14 @@ class Function:
         x = input.data
         y = self.forward(x)
         output = Variable(y)
+
+        self.input = input
         return output
 
     def forward(self, x: Any) -> Any:
+        raise NotImplementedError()
+
+    def backward(self, gy: Any) -> Any:
         raise NotImplementedError()
 
 
@@ -23,21 +29,34 @@ class Square(Function):
     def forward(self, x: Any) -> Any:
         return x**2
 
+    def backward(self, gy: Any) -> Any:
+        x = self.input.data
+        gx = 2 * x * gy
+        return gx
+
 
 class Exp(Function):
     def forward(self, x: Any) -> Any:
         return np.exp(x)
 
-
-def numerical_diff(f: Function, x: Variable, eps: float = 1e-4) -> float:
-    x0 = Variable(x.data - eps)
-    x1 = Variable(x.data + eps)
-    y0 = f(x0)
-    y1 = f(x1)
-    return (y1.data - y0.data) / (2 * eps)
+    def backward(self, gy: Any) -> Any:
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
 
 
-f = Square()
-x = Variable(np.array(2.0))
-dy = numerical_diff(f, x)
-print(dy)  # 4.000000000004
+A = Square()
+B = Exp()
+C = Square()
+
+x = Variable(np.array(0.5))
+a = A(x)
+b = B(a)
+y = C(b)
+
+y.grad = np.array(1.0)
+b.grad = C.backward(y.grad)
+a.grad = B.backward(b.grad)
+x.grad = A.backward(a.grad)
+
+print(x.grad)
