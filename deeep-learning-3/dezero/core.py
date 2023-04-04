2af4eeb15c00c108ea687470f29af4aab03edc07
kazuki <kaz.tokyo.tech20@gmail.com>
2023-04-04 14:27:31 +0900

    step2: add function

--- deeep-learning-3/dezero/core.py
+++ deeep-learning-3/dezero/core.py
@@ -1,5 +1,17 @@
 from typing import Any
 
+
 class Variable:
     def __init__(self, data: Any):
         self.data = data
+
+
+class Function:
+    def __call__(self, input: Variable) -> Variable:
+        x = input.data
+        y = self.forward(x)
+        output = Variable(y)
+        return output
+
+    def forward(self, x: Any) -> Any:
+        raise NotImplementedError()
