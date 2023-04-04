063c0fb6854e1d98439fbcb14ecb370a6d6c2f13
kazuki <kaz.tokyo.tech20@gmail.com>
2023-04-04 15:40:34 +0900

    add step9.2

---
+++ deeep-learning-3/steps/step9.2.py
@@ -0,0 +1,81 @@
+from typing import Any, Optional
+
+import numpy as np
+
+
+class Variable:
+    def __init__(self, data: Any):
+        self.data: Any = data
+        self.grad: Any = None
+        self.creator: Optional["Function"] = None
+
+    def set_creator(self, func: "Function") -> None:
+        self.creator = func
+
+    def backward(self) -> None:
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        creator_funcs: list[Optional["Function"]] = [self.creator]
+        while creator_funcs != []:
+            creator_func: Optional["Function"] = creator_funcs.pop()
+            if creator_func is not None:
+                # 関数の入出力を取得
+                x, y = creator_func.input, creator_func.output
+                x.grad = creator_func.backward(y.grad)
+
+                if x.creator is not None:
+                    creator_funcs.append(x.creator)
+
+
+class Function:
+    def __call__(self, input: Variable) -> Variable:
+        x = input.data
+        y = self.forward(x)
+        output = Variable(y)
+        output.set_creator(self)  # 出力変数に生みの親を覚えさせる
+
+        self.input = input
+        self.output = output  # 出力も覚える
+        return output
+
+    def forward(self, x: Any) -> Any:
+        raise NotImplementedError()
+
+    def backward(self, gy: Any) -> Any:
+        raise NotImplementedError()
+
+
+class Square(Function):
+    def forward(self, x: Any) -> Any:
+        return x**2
+
+    def backward(self, gy: Any) -> Any:
+        x = self.input.data
+        gx = 2 * x * gy
+        return gx
+
+
+class Exp(Function):
+    def forward(self, x: Any) -> Any:
+        return np.exp(x)
+
+    def backward(self, gy: Any) -> Any:
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
+
+
+def square(x: Variable) -> Variable:
+    return Square()(x)
+
+
+def exp(x: Variable) -> Variable:
+    return Exp()(x)
+
+
+x = Variable(np.array(0.5))
+y = square(exp(square(x)))
+y.backward()
+
+print(x.grad)
